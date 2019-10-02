93c8ef91205e8ff9909f31f8eb303561a54dcc00
Roberto <robpalacios1@hotmail.com>
2019-10-02 14:20:07 -0500

    do changes to slide

--- js/script.js
+++ js/script.js
@@ -1,17 +1,38 @@
-let content = document.querySelector('#content')
-function traer(){
-    try{
-      fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response => response.json())
-        .then(data => {
-            console.log(data['0'])
-            content.innerHTML = `
-            <p>Name: ${data['0'].name}</p>
-            <p>City: ${data['0'].address.city}</p>
-            <p>Address: ${data['0'].company.name}</p>
-            `
-        })
-    } catch(e) {
-      console.log('mensaje de error');
-    }
+let carousel = document.querySelector('#carousel');
+const getInitialData = async () => {
+  let BASE_URL = 'https://jsonplaceholder.typicode.com';
+  try {
+    let [reqUsers,reqPost ] = await Promise.all([
+      fetch(`${BASE_URL}/users`),
+      fetch(`${BASE_URL}/posts`)
+    ]);
+
+    let [dataUsers, dataPost] = await Promise.all([
+      reqUsers.json(),
+      reqPost.json()
+    ]);
+
+    let html = '';
+
+    dataUsers.some((element, index) => {
+      var active = index == 0 ? 'active' : '';
+      html += `
+        <div class="carousel-item ${active}">
+          <img class="user" src="./images/person_${index+1}.jpg" alt="${element.name}">
+          <h5>${element.name}</h5>
+          <p class="placework">${element.company.name}</p>
+          <p class="testimonial">"${dataPost[index].title}"</p>
+        </div>
+      `;
+      console.log(html)
+      return index == 3;
+    });
+
+    carousel.innerHTML = html;
+
+  } catch(e) {
+    console.log('error al traer datos');
+  }
 }
+
+getInitialData()
